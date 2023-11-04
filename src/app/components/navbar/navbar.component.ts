f054ca5256e8f991eaa98b15fa00a0ba74a210b1
fonCki <alfonsoridao@gmail.com>
2023-11-04 15:01:42 +0100

    this is beautifull, loving angular!

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { NavbarService } from '../../services/navbar.service';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
@@ -9,13 +11,20 @@ import { AuthService } from '../../services/auth.service';
 export class NavbarComponent {
   isLoggedIn = false;
   userPhoto: string | null = null;
+  showMiddleNav = true;
+  private searchExpandedSubscription: Subscription | undefined;
 
-  constructor(public authService: AuthService) {
+  constructor(public authService: AuthService, private navbarService: NavbarService) {
     this.authService.user.subscribe(user => {
       this.isLoggedIn = !!user;
       if (user) {
         this.userPhoto = user.photoURL;
       }
+      this.searchExpandedSubscription = this.navbarService.searchExpanded$.subscribe(
+        isExpanded => {
+          this.showMiddleNav = !isExpanded;
+        }
+      );
     });
   }
 
@@ -25,5 +34,11 @@ export class NavbarComponent {
       return user.displayName?.split(' ').map(name => name[0].toUpperCase()).join('') || '';
     }
     return '';
+  }
+
+  ngOnDestroy(): void {
+    if (this.searchExpandedSubscription) {
+      this.searchExpandedSubscription.unsubscribe();
+    }
   }
 }
